c7c01caa89c29279d7ae4ada046dca2717ed13be
Yath Seanghay <seanghay.dev@gmail.com>
2021-10-19 20:26:51 +0700

    fix(test): username

--- src/template/push.md
+++ src/template/push.md
@@ -1,5 +1,1 @@
-[{{sender.login}}]({{sender.html_url}}) pushed to [{{repository.full_name}}]({{repository.html_url}})
----
-{{#commits}}
-  • [{{ message }}]({{ url }})
-{{/commits}}
+{{sender.login}}
--- test/render.test.js
+++ test/render.test.js
@@ -4,6 +4,6 @@ const fixtures = require('./fixture');
 describe('render payload', () => {
   it('should return summary text', () => {
     const eventName = extractEventFromHeaders(fixtures.push.headers);
-    expect(render(eventName, fixtures.push.body)).toEqual(eventName);
+    expect(render(eventName, fixtures.push.body)).toEqual("baxterthehacker");
   })
 })
